155e545a3a2531a41311db6f1c278c75046e2742
JoseDeLaCruz28 <1josemaria09luis@gmail.com>
2023-10-01 13:44:56 -0500

    Nuevos cambios

---
+++ mostrarOcultarMenu.js
@@ -0,0 +1,10 @@
+//Esta funcion ocultará o mostrará el menu
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+let menuVisible = false;
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
